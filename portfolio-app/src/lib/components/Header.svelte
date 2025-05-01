39f7a61dc82ae1b718b62f6945603eb187c41206
Aleksejs Buciks <C00300235@setu.ie>
2025-05-01 22:27:51 +0100

    Name change

---
+++ portfolio-app/src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Navigation } from "$lib"
+</script>    
+
+<header>
+    <div class="logo">
+        <img src="/logo.png" alt="Website Logo">
+        <h1>My Svelte App</h1>
+    </div>
+
+    <Navigation />
+</header>
+
+<style>
+    
+</style>
---
+++ portfolio-app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+    import {Card} from '$lib';
+
+    onMount(() => {
+      console.log("Home Page Loaded");  
+    })
+</script>
+
+<div class="Heading">
+    
+<h1>Portfolio</h1>
+
+</div>
+
+
+<style>
+      
+  .Heading {
+    text-align: center;
+  }
+
+</style>    
